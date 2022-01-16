e799c41b40e4555b111d7483c21c56295a451164
jylee2 <leej1025y@live.com>
2022-01-16 21:41:06 +0800

    feat: add variables

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// MAIN TYPES
+	// string
+	// bool
+	// int
+	// int int8 int16 int32 int64
+	// uint uint8 uint16 uint32 uint64 uintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64 (the usual one)
+	// complex64 complex128
+
+	// Using var
+	// var name string = "Jun"
+	var age int32 = 37
+	const isCool = true
+	var size float32 = 2.3
+
+	// Shorthand inside a function
+	// name := "Jun"
+	// size := 1.3
+	// email := "asdf@gmail.com"
+
+	name, email := "Jun", "jun@example.com"
+
+	fmt.Println(name, email, age, isCool)
+	fmt.Printf("%T\n", size)
+}
